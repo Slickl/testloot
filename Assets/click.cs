e9ffb3c7e7a6bc6dd9c1d3df496ee06d3b2b8264
Slickl <schuetz_dennis@yahoo.de>
2017-08-28 13:50:07 +0200

    button attack funktioniert endlich
    tada

--- Assets/click.cs
+++ Assets/click.cs
@@ -1,33 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class click : MonoBehaviour
 {
 
-   
-
-    // Use this for initialization
     void Start()
     {
 
-        
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
-
-    }
-
-    // Button auf A wenn click dann attack
-    public void isClicked()
-    {
-
-        Application.LoadLevel("test");
-        Application.UnloadLevel("battle");
-        Debug.Log("You have clicked the button!");
+        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero);
+        if (hit != null && hit.collider != null && Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("I'm hitting " + hit.collider.name);
+        }
     }
 }
+
